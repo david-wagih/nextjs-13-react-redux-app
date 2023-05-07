7542de6ae1596bf858e3c052c10860342ce0d62d
davidwagih62@gmail.com <davidwagih62@gmail.com>
2023-05-07 16:18:32 +0300

    set project structure and packages needed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,80 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// want to design a login page
+"use client";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <Login />
+    </div>
+  );
+}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+function handleSubmit(e: any) {
+  e.preventDefault();
+  console.log(e.target.username.value);
+  console.log(e.target.password.value);
+}
+
+function Login() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "50%",
+        margin: "auto",
+      }}
+    >
+      <h1>Login</h1>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "50%",
+          margin: "auto",
+          justifyContent: "space-between",
+          alignContent: "center",
+        }}
+      >
+        <label htmlFor="username">Username</label>
+        <input
+          style={{
+            width: "100%",
+            padding: "5px",
+            marginTop: "10px",
+            marginBottom: "10px",
+            borderRadius: "5px",
+            border: "1px solid black",
+          }}
+          type="text"
+          id="username"
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          style={{
+            width: "100%",
+            padding: "5px",
+            marginTop: "10px",
+            marginBottom: "10px",
+            borderRadius: "5px",
+            border: "1px solid black",
+          }}
+        />
+        <button
+          type="submit"
+          style={{
+            width: "50%",
+            margin: "auto",
+            padding: "10px",
+            marginTop: "10px",
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
